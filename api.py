40879e67651e1b116ea5ded9d50975536ca3012e
Felipe Castillo Aravena <fcastilloaravena@gmail.com>
2024-10-16 16:20:00 +0200

    Resueltos conflictos tras permitir historias no relacionadas

--- api.py
+++ api.py
@@ -1,39 +1,31 @@
-
 from flask import Flask, request, jsonify
 import joblib
 import numpy as np
 from waitress import serve
-
-
 import warnings
 from sklearn.exceptions import InconsistentVersionWarning
 
 warnings.filterwarnings("ignore", category=InconsistentVersionWarning)
 
-
-# function to load the saved model
+# Function to load the saved model
 def load_model():
     model = joblib.load('Model/pkl/rf_model.pkl')
     return model
 
-# function to make predictions
-def predict(input_data):
-    model = load_model()
-    prediction = model.predict(input_data)
-    return prediction
-
+# Initialize Flask app
 app = Flask(__name__)
 
-# load the model
-model = joblib.load('Model/pkl/rf_model.pkl')
+# Load the model globally for efficiency
+model = load_model()
 
-# define a route for making predictions
+# Define a route for making predictions
 @app.route('/predict', methods=['POST'])
-def predict():
+def predict_route():
     data = request.get_json()  # Get input data from POST request
     input_data = np.array(data['input']).reshape(1, -1)  # Reshape input
     prediction = model.predict(input_data)
     return jsonify({'prediction': int(prediction[0])})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Serve the app using waitress
+    serve(app, host='0.0.0.0', port=8000)
